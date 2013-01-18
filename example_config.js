cad378bb0af3f0ca19194dda4dc4c959f18c4f3d
Stephan Schmidt <stephan.schmidt@gmail.com>
2013-01-18 11:40:37 +0100

    Added example with all config option

---
+++ example_config.js
@@ -0,0 +1,20 @@
+var _seo = {
+	'em_check': true,
+	'strong_check': true,
+	'h_check': true,
+	'keywords_check': true,
+	'description_check': true,
+	'title_check': true,
+
+	'h1_min': 1,
+	'h1_max': 1,
+	'h2_min': 1,
+	'h3_min': 1,
+	'keywords_min': 1,
+	'keywords_max': 5,
+	'description_min': 70,
+	'description_max': 160,
+	'title_max': 10,
+	'title_max': 70
+
+};
